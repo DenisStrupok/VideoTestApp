8ae04c50cc613e29082d74baae1e091bf4f19a66
denys.strupok <denys.strupok@stfalcon.com>
2023-01-28 05:33:20 +0200

    Init project

---
+++ app/src/main/java/com/example/videotestapp/features/detail/DetailFragment.kt
@@ -0,0 +1,13 @@
+package com.example.videotestapp.features.detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.videotestapp.R
+
+class DetailFragment: Fragment(R.layout.fragment_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/videotestapp/features/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.videotestapp.features.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.videotestapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
